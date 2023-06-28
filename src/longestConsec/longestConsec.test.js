730fb99374c0143b4ba585143589920e53cb3711
Carlos Rodriguez <carlos15erh@gmail.com>
2023-06-27 19:43:57 -0500

    update file names

--- src/longestConsec/longestConsec.test.js
+++ src/longestConsec/longestConsec.test.js
@@ -1,31 +1,31 @@
-import longestString from './longestString';
+import longestConsec from './longestConsec';
 
 describe('Basic tests', () => {
   test('should return => abc', () => {
     const variable = ['a', 'b', 'c', 'd'];
     const r = 3;
-    const result = longestString(variable, r);
+    const result = longestConsec(variable, r);
     expect(result).toBe('abc');
   });
 
   test('should return => efghijklmnop', () => {
     const variable = ['abcd', 'efghij', 'klmnop', 'qrst', 'cuenta', 'uvwxyz'];
     const r = 2;
-    const result = longestString(variable, r);
+    const result = longestConsec(variable, r);
     expect(result).toBe('efghijklmnop');
   });
 
   test('should return => eduardorodriguez', () => {
     const variable = ['carlos', 'eduardo', 'rodriguez', 'huerta'];
     const r = 2;
-    const result = longestString(variable, r);
+    const result = longestConsec(variable, r);
     expect(result).toBe('eduardorodriguez');
   });
 
   test('should return => null', () => {
     const variable = ['carlos', 'eduardo', 'rodriguez', 'huerta'];
     const r = 1;
-    const result = longestString(variable, r);
+    const result = longestConsec(variable, r);
     expect(result).toBe('rodriguez');
   });
 });
@@ -34,21 +34,21 @@ describe('Corner cases', () => {
   test('should return => null', () => {
     const variable = ['carlos', 'eduardo', 'rodriguez', 'huerta'];
     const r = 0;
-    const result = longestString(variable, r);
+    const result = longestConsec(variable, r);
     expect(result).toBeNull();
   });
 
   test('should return => null', () => {
     const variable = ['carlos', 'eduardo', 'rodriguez', 'huerta'];
     const r = 5;
-    const result = longestString(variable, r);
+    const result = longestConsec(variable, r);
     expect(result).toBeNull();
   });
 
   test('should return => null', () => {
     const variable = ['abcd', 'efghij', 'klmnop', 'qrst', 'cuenta', 'uvwxyz'];
     const r = 10;
-    const result = longestString(variable, r);
+    const result = longestConsec(variable, r);
     expect(result).toBeNull();
   });
 });
